bad0737262cd6578220d7be410d7098accdf557b
ceritaMustofa <futu.bontet@gmail.com>
2022-12-02 13:51:49 -0800

    add custom permissions

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path("api/v1/", include("posts.urls")),
     path('admin/', admin.site.urls),
+    path("api/v1/", include("posts.urls")),
+    path("api-auth/", include("rest_framework.urls")),
 ]
