383f3eee6f8fd03b79fbdb2a0a1a12903ea21648
ceritaMustofa <futu.bontet@gmail.com>
2022-12-03 17:37:04 -0800

    add user authentication

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -27,8 +27,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "django.contrib.sites",
     # 3rd-party apps
     "rest_framework",
+    "rest_framework.authtoken",
+    "corsheaders",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    'dj_rest_auth',
+    'dj_rest_auth.registration',
     # Local
     "accounts.apps.AccountsConfig",
     "posts.apps.PostsConfig",
@@ -37,6 +45,10 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated"
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
     ]
 }
 
@@ -73,10 +85,15 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                "django.template.context_processors.request"
             ],
         },
     },
 ]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+SITE_ID = 1
 
 WSGI_APPLICATION = 'blog_project.wsgi.application'
 
